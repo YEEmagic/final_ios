c4ca58af9324358f0e263749e84d036127758cbe
YEEmagic <110275423+YEEmagic@users.noreply.github.com>
2024-06-19 10:02:01 +0900

    Add files via upload

---
+++ Final/Final/TableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  dsfafasfasf
+//
+//  Created by 232-09 on 2024/06/19.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    var photo = ["spring.png","summer.png","autumn.png","winter.png"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        UITableView(frame: photo) {
+//
+//        }
+        // Do any additional setup after loading the view.
+        
+        
+        
+        
+    }
+
+
+}
+
---
+++ Final/Final/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  asdf
+//
+//  Created by 232-09 on 2024/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//   @IBAction func btnChangeBg(_ sender: UIButton) {
+//        if btnChangeBg(UIButton) {
+//            UIColor(cgColor: Black)
+//        }
+//    }
+    
+}
+
